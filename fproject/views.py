d106d68d69692b416a42c342180eb3d03affcad5
Raian <raiannicacio@id.uff.br>
2019-06-03 09:43:18 -0300

    Update of Template and Visualization

--- fproject/views.py
+++ fproject/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post, Goal
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'fproject/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')
+	goals = Goal.objects.all()
+	return render(request, 'fproject/post_list.html', {'posts': posts, 'goals': goals})
