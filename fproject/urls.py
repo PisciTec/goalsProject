fc4d5411805151a105aa3bb312c103e34f59fde9
Raian <raiannicacio@id.uff.br>
2019-06-03 23:05:28 -0300

    Fix in Goals Views

--- fproject/urls.py
+++ fproject/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name = 'post_list')
+	path('', views.post_list, name = 'post_list'),
+	path('goals', views.goal_list, name = 'goal_list')
 ]
--- fproject/views.py
+++ fproject/views.py
@@ -7,3 +7,6 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')
 	goals = Goal.objects.all()
 	return render(request, 'fproject/post_list.html', {'posts': posts, 'goals': goals})
+def goal_list(request):
+	goals = Goal.objects.all()
+	return render(request, 'fproject/goals.html', {'goals': goals})
